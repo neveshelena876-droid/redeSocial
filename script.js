e78245197317642c3ac157eba8466717b93ef085
neveshelena876-droid <neveshelena876@gmail.com>
2026-09-09 11:03:43 -0300

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoeaded"),()=>{(
+    const likeBtn = document.querySelector(like-btm);
+    const postMidia = document.querySelector(.post-midia);
+    if(!likeBtn) return;
+
+    const likescontSpan = likeBtn.querySelector(".likes-count");
+    const bookmarBtn = document.querySelector(".bookmark-btn");
+
+    let isliked = false;
+    let baseLikes = 0;
+
+)
+}
